d158b229adfdd97379f24bf5e805daeec8219c58
LizVasquez <lizvasmen@gmail.com>
2021-08-21 09:03:58 -0400

    18/08/2021 pipes of angular

--- src/app/pages/admin/admin1/admin1.component.ts
+++ src/app/pages/admin/admin1/admin1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-admin1',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Admin1Component implements OnInit {
 
+  name = 'liz';
+
+  auxJSON = {name: 'Luz', lastname: 'mendoza'};
+
+  auxObs$ = new BehaviorSubject<number[]>([1,2,3]);
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onAddPipe(){
+    const aux = this.auxObs$.getValue();
+    const aux1 = [4,5].concat(aux);
+    this.auxObs$.next(aux1);
+    }
 
 }
