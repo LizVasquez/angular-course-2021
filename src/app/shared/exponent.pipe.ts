48ebbdef42adedca39c59c7ce5f0c82cea52ae73
LizVasquez <lizvasmen@gmail.com>
2021-08-22 07:15:43 -0400

    20/08/2021 exponent pipe built

--- src/app/shared/exponent.pipe.ts
+++ src/app/shared/exponent.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+/* CLASE 20/08  */
 
 @Pipe({
   name: 'exponent'
 })
 export class ExponentPipe implements PipeTransform {
 
-  transform(value: number): number {
+  /*transform(value: number): number {
     return value *2;
+  }*/
+
+  /*transform(value: number, exponent: number): number {
+    return Math.pow(value, exponent);
+  }*/
+
+  transform(value: number, exponent?:number): number {
+    return Math.pow(value, isNaN(exponent) ? 1 : exponent);
   }
 
 }
