e91e8b3a0792d257de7e8ec3a1afaca4c57f2e33
LizVasquez <lizvasmen@gmail.com>
2021-09-03 19:38:06 -0400

    etst 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { SingletonService } from './login/services/singleton.service';
 
 
 
 
 
-const routes: Routes=[
-  {path: '', redirectTo:'login', pathMatch: 'full'},
-  {path: 'login', loadChildren: ()=>
-    import('./login/login.module').then(m => m.LoginModule)},
-  
-  {path: 'pages', loadChildren: ()=>
-    import('./pages/pages.module').then(m => m.PagesModule)}
-
-]
-    
 
 @NgModule({
   declarations: [
@@ -28,8 +17,7 @@ const routes: Routes=[
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    BrowserAnimationsModule
    
   ],
   providers: [],
