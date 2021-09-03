5c5c77e1efa4ef3d59e9e4ec4faefb59803ff276
LizVasquez <lizvasmen@gmail.com>
2021-09-03 19:54:36 -0400

    tst 4

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { SingletonService } from 'src/app/login/services/singleton.service';
+
+const routes: Routes= [
+  {path: '', component: HomeComponent}
+
+]
+
+
+
+@NgModule({
+  declarations: [
+   HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [
+    
+  ]
+})
+export class HomeModule { }
