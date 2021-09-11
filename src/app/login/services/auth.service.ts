98ec6d9c7d77dfabb01db66e51d40387a035d94f
LizVasquez <lizvasmen@gmail.com>
2021-09-10 20:55:06 -0400

    09/09/2021 save token - localstorage

--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
@@ -12,12 +13,24 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   public login(body:any): Observable<any>{
-    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body);
+    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(
+      map( (res:any) => {
+        this.authSuccess(res.idToken, res.localId)
+        return res;
+      })
+    );
 
   }
 
   public signUp(body:any): Observable<any>{
     return this.http.post(`${this.url}/v1/accounts:signUp?key=${this.key}`, body);
+
+  }
+
+  private authSuccess(token:string, userId: string){
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
+
 
   }
 }
